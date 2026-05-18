5f1872d8cb848f37a0a890f360e46f27b43e4876
shrutihansaliya <shrutihansaliya2005@gmail.com>
2026-05-18 12:36:18 +0530

    Admin overview page and TT coordinator overview page

--- ScheduleX.Web/Services/TT/TTOverviewService.cs
+++ ScheduleX.Web/Services/TT/TTOverviewService.cs
@@ -11,30 +11,75 @@ public class TTOverviewService
         _context = context;
     }
 
-    public async Task<OverviewDto> GetOverview(int courseId, int termId, int yearId)
+    public async Task<OverviewDto> GetOverview(
+     int courseId,
+     int academicTermId,
+     int academicYearId)
     {
-        var totalFaculty = await _context.Faculties
-            .Where(x => x.IsActive)
+        var totalFaculty = await _context.SubjectFaculties
+            .Where(x =>
+                x.AcademicYearId == academicYearId &&
+                x.Division.CourseId == courseId &&
+                x.IsActive)
+            .Select(x => x.FacultyId)
+            .Distinct()
             .CountAsync();
 
-        var availableFaculty = await _context.Faculties
-            .Where(x => x.IsActive)
-            .CountAsync(x => x.FacultyAvailabilities.Any());
+        var availableFaculty = await _context.SubjectFaculties
+            .Where(x =>
+                x.AcademicYearId == academicYearId &&
+                x.Division.CourseId == courseId &&
+                x.IsActive &&
+                x.Faculty.IsActive)
+            .Select(x => x.FacultyId)
+            .Distinct()
+            .CountAsync();
 
-        var totalSubjects = await _context.Subjects
-            .Where(x => x.IsActive && x.CourseId == courseId)
+        var totalSubjects = await _context.SubjectSemesters
+            .Where(x =>
+                x.AcademicYearId == academicYearId &&
+                x.Subject.CourseId == courseId &&
+                x.IsActive)
             .CountAsync();
 
         var totalRooms = await _context.Rooms
             .Where(x => x.IsActive)
             .CountAsync();
 
+        var totalDivisions = await _context.Divisions
+            .Where(x =>
+                x.AcademicYearId == academicYearId &&
+                x.CourseId == courseId &&
+                x.IsActive)
+            .CountAsync();
+
+        var totalTemplates = await _context.TimeTableTemplates
+            .Where(x => x.IsActive)
+            .CountAsync();
+
+        var generatedCount = await _context.TimeTableBatches
+            .Where(x =>
+                x.AcademicYearId == academicYearId &&
+                x.AcademicTermId == academicTermId &&
+                x.CourseId == courseId)
+            .CountAsync();
+
+        var hasScheduleConfig = await _context.ScheduleConfigs
+            .AnyAsync(x =>
+                x.AcademicYearId == academicYearId &&
+                x.CourseId == courseId &&
+                x.IsActive);
+
         return new OverviewDto
         {
             TotalFaculty = totalFaculty,
             AvailableFaculty = availableFaculty,
             TotalSubjects = totalSubjects,
-            TotalRooms = totalRooms
+            TotalRooms = totalRooms,
+            TotalDivisions = totalDivisions,
+            TotalTemplates = totalTemplates,
+            GeneratedCount = generatedCount,
+            HasScheduleConfig = hasScheduleConfig
         };
     }
 }
